eb67b5d1beaa1aee3988b099ffda3fee2123644b
Vishnubansal51 <bansalvishnu51@gmail.com>
2023-07-26 09:55:18 +0530

    Done

--- client/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import authSlice from './features/auth/authSlice'
-
-const store = configureStore({
-    reducer:{
-        auth: authSlice.reducer, 
-    }
-})
-export default store
